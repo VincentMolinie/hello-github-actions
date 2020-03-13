6c010abb5471334e6585a43359e75ad0fbbcc316
Vincent Molinié <vincent.molinie@epita.fr>
2020-03-13 14:59:15 +0100

    Update action

--- scripts/update-clickup-status.js
+++ scripts/update-clickup-status.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 
 const file = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
+const eventPayload = JSON.parse(file);
+
+if (eventPayload && eventPayload.pull_request && eventPayload.pull_request.title) {
+  if (eventPayload.pull_request.title.includes('(#')) {
+    const index = eventPayload.pull_request.title.indexOf('(#');
+    const clickUpTag = eventPayload.pull_request.title.substring(index, index + 6);
+
+    console.log(' =========== Click Up Tag', clickUpTag, '=============');
+  }
+}
 console.log(file);
