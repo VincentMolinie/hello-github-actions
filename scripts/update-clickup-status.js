c1da4f57b4290209094da60849d220b9cf2d6685
Vincent Molinié <vincent.molinie@epita.fr>
2020-03-13 15:01:41 +0100

    Fix lickup tag

--- scripts/update-clickup-status.js
+++ scripts/update-clickup-status.js
@@ -5,7 +5,7 @@ const eventPayload = JSON.parse(file);
 
 if (eventPayload && eventPayload.pull_request && eventPayload.pull_request.title) {
   if (eventPayload.pull_request.title.includes('(#')) {
-    const index = eventPayload.pull_request.title.indexOf('(#');
+    const index = eventPayload.pull_request.title.indexOf('(#') + 2;
     const clickUpTag = eventPayload.pull_request.title.substring(index, index + 6);
 
     console.log(' =========== Click Up Tag', clickUpTag, '=============');
