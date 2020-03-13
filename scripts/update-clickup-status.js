31706ee645e449cab2842c19ae87438bd403b74c
Vincent Molinié <vincent.molinie@epita.fr>
2020-03-13 15:35:04 +0100

    Check if is in review

--- scripts/update-clickup-status.js
+++ scripts/update-clickup-status.js
@@ -5,6 +5,24 @@ const CLICKUP_API_KEY = 'pk_4518205_4TA4J4EYV5R2LSP67ZK7K5KQ63S79MTG';
 const file = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
 const eventPayload = JSON.parse(file);
 
+if (eventPayload.pull_request
+  && eventPayload.pull_request.requested_reviewers
+  && eventPayload.pull_request.requested_reviewers.length
+  && eventPayload.pull_request.assignees
+  && eventPayload.pull_request.assignees.length
+) {
+  const reviewerLogins = eventPayload.pull_request.requested_reviewers.map(
+    reviewer => reviewer.login
+  );
+  const assigneeLogins = eventPayload.pull_request.assignees.map(
+    assignee => assignee.login
+  );
+
+  const isInReview = assigneeLogins.some(assigneeLogin => reviewerLogins.some(reviewerLogin => reviewerLogin === assigneeLogin));
+
+  console.log(' ===== isInReview', isInReview);
+}
+
 if (eventPayload && eventPayload.pull_request && eventPayload.pull_request.title) {
   if (eventPayload.pull_request.title.includes('(#')) {
     const index = eventPayload.pull_request.title.indexOf('(#') + 2;
@@ -20,7 +38,7 @@ if (eventPayload && eventPayload.pull_request && eventPayload.pull_request.title
         'Authorization': CLICKUP_API_KEY,
       }
     }).then(response => {
-      console.log('response', response);
+      console.log('_______________ response\n', response.data);
     })
   }
 }
