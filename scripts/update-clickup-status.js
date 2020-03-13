c195ad2c9e815dc9bbcde2ce677d2b51f3b304b9
Vincent Molinié <vincent.molinie@epita.fr>
2020-03-13 15:10:25 +0100

    Try to get card from clikup

--- scripts/update-clickup-status.js
+++ scripts/update-clickup-status.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const axios = require('axios');
+const CLICKUP_API_KEY = 'pk_4518205_4TA4J4EYV5R2LSP67ZK7K5KQ63S79MTG';
 
 const file = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
 const eventPayload = JSON.parse(file);
@@ -9,6 +11,17 @@ if (eventPayload && eventPayload.pull_request && eventPayload.pull_request.title
     const clickUpTag = eventPayload.pull_request.title.substring(index, index + 6);
 
     console.log(' =========== Click Up Tag', clickUpTag, '=============');
+
+    axios({
+      method: 'GET',
+      url: `https://api.clickup.com/api/v2/task/${clickUpTag}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': CLICKUP_API_KEY,
+      }
+    }).then(response => {
+      console.log('response', response);
+    })
   }
 }
 console.log(file);
